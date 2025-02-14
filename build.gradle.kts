9021cbd0591f996941e8a6d3fb7d32237511e537
FerrumBrain <lemur1416@gmail.com>
2025-02-14 11:28:24 +0100

    removed local publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -152,10 +152,6 @@ project.pluginManager.withPlugin("maven-publish") {
             }
             repositories {
                 maven {
-                    name = "local"
-                    url = uri("/Users/Timur.Kudashev/IdeaProjects/casr-adapter/aboba")
-                }
-                maven {
                     url = uri("https://maven.pkg.github.com/plan-research/kotlin-maven")
                     credentials {
                         username = project.findProperty("gpr.user")?.toString()
